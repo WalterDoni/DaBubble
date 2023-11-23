89ea634a08183a4656685b45baadf62389d868c0
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2023-11-23 15:03:26 +0100

    start with mainboard

---
+++ src/app/models/channel.ts
@@ -0,0 +1,22 @@
+export class Channel {
+
+    name!: string;
+    description!: string;
+
+
+    toJSON(): any {
+        return {
+           name: this.name,
+           description: this.description,
+
+        }
+    }
+
+    constructor(object?: any){
+        this.name = object ? object.name: ''; 
+        this.description = object ? object.desciption: '';
+       
+     ;
+    }
+
+}
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,19 +2,25 @@ export class User {
 
     username!: string;
     email!: string;
-    password!: string;
+    deafaultImg!: string;
+    personalImg!: string;
+
 
     toJSON(): any {
         return {
             username: this.username,
             email: this.email,
-            password: this.password,
+            defaultImg: this.deafaultImg,
+            personalImg: this.personalImg,
+   
         }
     }
 
     constructor(object?: any){
         this.username = object ? object.username: ''; //fast if / else request --> if object exist object.name else ''
         this.email = object ? object.email : '';
-        this.password = object ? object.password : '';
+        this.deafaultImg = object ? object.defaultImg : '';
+        this.personalImg = object ? object.personalImg : '';
+     ;
     }
 }
