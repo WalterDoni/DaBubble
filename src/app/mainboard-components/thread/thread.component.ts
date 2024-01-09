8f7596091d9547d3e63adf86c73f9ada7c294d16
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-01-09 14:56:02 +0100

    answer thread

--- src/app/mainboard-components/thread/thread.component.ts
+++ src/app/mainboard-components/thread/thread.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { debugErrorMap } from '@angular/fire/auth';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { Firestore, collection, doc, updateDoc } from '@angular/fire/firestore';
 import { MainboardComponent } from 'src/app/mainboard/mainboard.component';
 
 @Component({
@@ -9,17 +9,18 @@ import { MainboardComponent } from 'src/app/mainboard/mainboard.component';
 })
 
 export class ThreadComponent {
+  @ViewChild('inputfieldValue') inputfieldValue!: ElementRef;
 
   selectedThreadID!: number;
   selectedChannelTitle!: string;
   selectedChannel: any[] = [];
-  selectedChannelContent: any[] = [];
+
+  firestore: Firestore = inject(Firestore);
 
   constructor(private mainboard: MainboardComponent) {
     this.selectedChannel = this.mainboard.selectedChannelContent;
     this.selectedThreadID = this.mainboard.hoveredChannelIndex;
     this.selectedChannelTitle = this.mainboard.selectedChannelTitle;
-    this.selectedChannelContent = this.mainboard.selectedChannelContent;
   }
 
   closeThread() {
@@ -31,4 +32,23 @@ export class ThreadComponent {
     return user ? user.img : '/assets/img/signup/profile.png';
   }
 
+  async addCommentInThread() {
+    let answerFromArray = this.selectedChannel[this.selectedThreadID]['data']['answerFrom'];
+    let messageArray = this.selectedChannel[this.selectedThreadID]['data']['answerText'];
+    let timeArray = this.selectedChannel[this.selectedThreadID]['data']['answerTime'];
+    answerFromArray.push(this.mainboard.loggedInUserName);
+    let messageValue = this.inputfieldValue.nativeElement.value;
+    messageArray.push(messageValue);
+    let newTime = this.mainboard.getCurrentTimeInMEZ();
+    timeArray.push(newTime);
+    await updateDoc(doc(this.channelContentRef(), this.selectedChannel[this.selectedThreadID]['id']), {
+      answerFrom: answerFromArray,
+      answerText: messageArray,
+      answerTime: timeArray,
+    })
+  }
+
+  channelContentRef() {
+    return collection(this.firestore, 'channels', this.mainboard.channelID, 'channelContent')
+  }
 }
